0eb573e4d7309aac3618eb354bd71e10b788191b
honesty-yoo <yhm9207@naver.com>
2022-02-17 23:18:47 +0900

    배치 init config
    - Job, Step 기본설정 추가

---
+++ src/main/java/com/example/batch/config/job/JobConfig.java
@@ -0,0 +1,35 @@
+package com.example.batch.config.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor    // 생성자 주입
+public class JobConfig {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job printJob() {
+        return jobBuilderFactory.get("printJob")
+                .start(printStep1())
+                .build();
+    }
+
+    @Bean
+    public Step printStep1() {
+        return stepBuilderFactory.get("printStep1")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("print step 1");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+}
